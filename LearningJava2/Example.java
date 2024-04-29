892754e6083f4d816f291eadf16dfd0d5a0b93e4
lou-i0 <lothen123@googlemail.com>
2024-04-29 09:30:46 +0100

    stuff

--- LearningJava2/Example.java
+++ LearningJava2/Example.java
@@ -5,6 +5,6 @@ This is a sample Java Program
 public class Example {
     
     public static void main(String[] args) {
-        System.out.println(" Java drvies the web");
+        System.out.println(" Java drives the web");
     }
 }
